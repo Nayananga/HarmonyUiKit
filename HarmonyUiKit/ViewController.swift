756d2558b95e0476621260af5424f7d095d146b1
nayanangamuhandiram@macos10.13vmware <nayanangamuhandiram@gmail.com>
2018-12-01 23:54:23 -0800

    layout 1

--- HarmonyUiKit/ViewController.swift
+++ HarmonyUiKit/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Outlets
+    @IBOutlet weak var loginButtonView: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.loginButtonSetup()
     }
-
-
+    
+  
+    func loginButtonSetup(){
+        self.loginButtonView.layer.shadowColor = UIColor(displayP3Red: 0, green: 0, blue: 0, alpha: 	0.9).cgColor
+        self.loginButtonView.layer.shadowRadius = 3.0
+        self.loginButtonView.layer.shadowOpacity = 0.2
+        self.loginButtonView.layer.shadowOffset = CGSize(width: 0.0, height: 12.0)
+        self.loginButtonView.layer.cornerRadius = self.loginButtonView.bounds.size.height/2
+        
+    }
+    
 }
 
